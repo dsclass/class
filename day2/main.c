ba4922765bcf2d5698cf459eda77e2a91790724c
ds <dougsdev@gmail.com>
2014-04-23 14:14:25 -0700

    blah

--- day2/main.c
+++ day2/main.c
@@ -2,16 +2,17 @@
 
 int asmfunction(void);
 
-void win(int a, int b)
+int win(int a, int b)
 {
+    int a = 5;
+    int b = 6;
+
     printf("You win %d %d\n", a, b);
+    return 9;
 }
 
 int main(void)
 {
-	int num;
-
-	num = asmfunction();
-	printf("Your function returned %d\n", num);
+    win(1,2);
 	return 0;
 }
